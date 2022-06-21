d9d8b071960e95e273f3767667efc95c7e800d8d
Jinkyum Park <jinpark1025@gmail.com>
2022-06-21 23:18:28 +0900

    remove head branch files

--- income-management/src/main/java/com/jinkyumpark/incomemanagement/income/IncomeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jinkyumpark.incomemanagement.income;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface IncomeRepository extends JpaRepository<Income, Long> {
-    List<Income> getIncomeById(Long id);
-}
--- income-management/src/main/java/com/jinkyumpark/incomemanagement/member/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jinkyumpark.incomemanagement.member;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface MemberRepository extends CrudRepository<Member, Long> {
-    Optional<Member> findByEmail(String email);
-}
